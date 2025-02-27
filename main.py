f006de57543334264aa75f31683888b2ce8dcb22
vazquala000 <vazquala000@warren.k12.in.us>
2025-02-27 09:39:21 -0500

    initial

---
+++ main.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    bank_name = "Fred's Bank"
+    num_accounts = 0
+
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+        BankAccount.num_accounts += 1
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    @classmethod
+    def display_bank_info(cls):
+        print(f"Bank Name: {cls.bank_name}, Number of Account: {cls.num_accounts}")
+
+
+bank_account_1 = BankAccount("Fred", 100)
+bank_account_2 = BankAccount("Susie", 200)
+bank_account_3 = BankAccount("Billie", 300)
+BankAccount.display_bank_info()
